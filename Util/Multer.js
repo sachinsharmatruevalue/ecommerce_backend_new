b23a829dcf11b155a18704c5811fb991960e96c2
sachin96sharma <104970567+sachin96sharma@users.noreply.github.com>
2025-03-18 10:15:54 +0530

    change multer file

--- Util/Multer.js
+++ Util/Multer.js
@@ -5,7 +5,7 @@ const path = require('path');
 // Configure multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, 'Uploads/');
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
